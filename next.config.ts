3be791a1c365da93dba77c82a09f0989ab7d485b
mdimrankhan218939 <misukkhanimran@gmail.com>
2026-05-24 15:09:29 +0300

    fix: run prisma generate before next build for vercel

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,7 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
+images: {
     remotePatterns: [
       {
         protocol: "https",
